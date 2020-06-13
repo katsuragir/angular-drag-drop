1194aa021d0631947a72c143a414f1a4efe28deb
Ridho Gumelar Bagaskara <47409331+ridhliver@users.noreply.github.com>
2020-06-13 20:22:52 +0700

    angular-drag-drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dragenddrop';
+
+  timePeriods = [
+    'Bronze age',
+    'Iron age',
+    'Middle ages',
+    'Early modern period',
+    'Long nineteenth century'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray([], event.previousIndex, event.currentIndex);
+  }
 }
